b0282fc8d61bc61f2e89428985f2fb3529976372
unlimiTheme <unlimitheme@gmail.com>
2022-10-05 17:19:13 +0300

    increased version

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'LIMEASYBLOG_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'LIMEASYBLOG_VERSION', '1.0.3' );
+	define( 'LIMEASYBLOG_VERSION', '1.0.4' );
 }
 
 if ( ! defined( 'LIMEASYBLOG_TEMPLATE_DIRECTORY_URI' ) ) {
